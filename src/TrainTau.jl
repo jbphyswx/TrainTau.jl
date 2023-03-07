afa24c6e1245df9905ec387ca8c82e157f0986a2
jbphyswx <jbenjami@caltech.edu>
2023-03-06 17:00:07 -0800

    Working state for training tau with log10 formulation

--- src/TrainTau.jl
+++ src/TrainTau.jl
@@ -1,5 +1,34 @@
 module TrainTau
 
-greet() = print("Hello World!")
+using Pkg
+Pkg.develop(path="/home/jbenjami/bin/git/SlurmTools.jl")
+Pkg.develop(path="/home/jbenjami/Research_Schneider/CliMa/TurbulenceConvection.jl")
+
+using Serialization
+using SlurmTools
+using NCDatasets
+using Dierckx
+using LinearAlgebra
+using Random
+using JLD2
+using Pickle
+using Serialization
+using PyCall
+using HDF5
+
+# using Flux
+using Optim
+using Statistics
+using ProgressMeter
+
+FT = Float64
+
+package_dir = dirname(@__DIR__)# parent of parent...
+
+include("SOCRATES/SLURM_submit_SOCRATES.jl")
+include("SOCRATES/run_all_SOCRATES.jl")
+include("SOCRATES/loss_SOCRATES.jl")
+include("SOCRATES/SOCRATES_Workflow.jl")
+include("SOCRATES/slurm_parallel_test.jl")
 
 end # module TrainTau
--- src/tau_formulations.jl
+++ src/tau_formulations.jl
@@ -10,9 +10,15 @@ function tau1(b::FT,c::FT,d::FT, ts, q_tot)
     b * (q_liq)
     c * (q_ice)
     d * T
+
+    # b,c,d are vectors with two values one for liq one for ice...
     """
 
-    return [b,c,d]' * [T = TD.air_temperature(ts), q_tot.liq, q_tot.ice]
+    arr = vcat(a',b',c')
+
+
+
+    return arr' * [T = TD.air_temperature(ts), q_tot.liq, q_tot.ice]
 
 end
 
@@ -28,4 +34,27 @@ function tau1(b::FT,c::FT,d::FT, locals)
     q_tot = locals[:q_tot]
     return tau1(b,c,d, ts, q_tot)
 
+end
+
+
+
+function tau2(locals; tau_liq=10, tau_ice,10 )
+    """
+    """
+    return [tau_liq, tau_ice]
+end
+
+function tau2(w;locals)
+    return w[1], w[2] 
+end
+
+
+function tau3(w,locals) # train in log space... (probably more stable...)
+    return 10^w[1], 10^w[2] 
+end
+
+
+
+function tau_log(w;locals)
+    return w[1], w[2] 
 end
